dbbb2a7e5cf2313c674341b4e371544633688427
Karl Southern <karl@theangryangel.co.uk>
2019-12-31 22:32:19 +0000

    WIP

--- src/wire.rs
+++ src/wire.rs
@@ -1,17 +1,18 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::str;
 
 pub struct RawMsg {
-    tags: Option<HashMap<String, Option<String>>>,
+    tags: Option<BTreeMap<String, Option<String>>>,
     source: Option<String>,
     command: String,
     params: Vec<String>,
 }
 
 impl RawMsg {
-    fn decode(x: String) -> RawMsg {
+    fn from_string(x: String) -> RawMsg {
         let mut i = x.trim().chars().fuse().peekable();
 
-        let tags: Option<HashMap<String, Option<String>>> = if i.peek() == Some(&'@') {
+        let tags: Option<BTreeMap<String, Option<String>>> = if i.peek() == Some(&'@') {
             let tags_string = i.by_ref().skip(1).take_while(|c| c != &' ').collect::<String>();
 
             Some(
@@ -59,6 +60,45 @@ impl RawMsg {
 
         RawMsg{tags: tags, source: source, command: command, params: params}
     }
+
+    fn to_string(&self) -> String {
+        let mut s = String::new();
+
+        if !self.tags.is_none() {
+            let tags = self.tags.as_ref().unwrap().iter().map(|kv|
+                if kv.1.is_none() {
+                    format!("{}", kv.0)
+                } else {
+                    format!("{}={}", kv.0, kv.1.as_ref().unwrap())
+                }
+            )
+            .collect::<Vec<String>>();
+
+            if tags.len() > 0 {
+                s.push_str(format!("@{} ", tags.join(";")).as_ref());
+            }
+        }
+        
+
+        if !self.source.is_none() {
+            s.push_str(format!(":{} ", self.source.as_ref().unwrap()).as_ref());
+        }
+
+        s.push_str(&self.command);
+        
+        for param in &self.params {
+            if param.contains(" ") {
+                s.push_str(" :");
+            } else {
+                s.push_str(" ");
+            }
+
+            s.push_str(&param);
+        }
+
+        s.push_str("\r\n");
+        s
+    }
 }
 
 #[cfg(test)]
@@ -66,10 +106,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn complete_test() {
-        let sample = String::from("@id=234AB;rose :dan!d@localhost PRIVMSG #chan :Hey what's up!");
+    fn from_string_complete_test() {
+        let sample = String::from("@id=234AB;rose :dan!d@localhost PRIVMSG #chan :Hey what's up!\r\n");
 
-        let msg = RawMsg::decode(sample);
+        let msg = RawMsg::from_string(sample);
         let tags = msg.tags.unwrap();
         let source = msg.source.unwrap();
         let command = msg.command;
@@ -100,10 +140,10 @@ mod tests {
     }
 
     #[test]
-    fn no_tags_test() {
-        let sample = String::from(":irc.example.com CAP LS * :multi-prefix extended-join sasl");
+    fn from_string_no_tags_test() {
+        let sample = String::from(":irc.example.com CAP LS * :multi-prefix extended-join sasl\r\n");
 
-        let msg = RawMsg::decode(sample);
+        let msg = RawMsg::from_string(sample);
         let source = msg.source.unwrap();
 
         assert_eq!(None, msg.tags);
@@ -116,10 +156,10 @@ mod tests {
     }
 
     #[test]
-    fn no_tags_no_source_test() {
-        let sample = String::from("CAP LS * :multi-prefix extended-join sasl");
+    fn from_string_no_tags_no_source_test() {
+        let sample = String::from("CAP LS * :multi-prefix extended-join sasl\r\n");
 
-        let msg = RawMsg::decode(sample);
+        let msg = RawMsg::from_string(sample);
 
         assert_eq!(None, msg.tags);
         assert_eq!(None, msg.source);
@@ -131,10 +171,10 @@ mod tests {
     }
 
     #[test]
-    fn no_tags_no_trailing_test() {
-        let sample = String::from(":dan!d@localhost PRIVMSG #chan Hey!");
+    fn from_string_no_tags_no_trailing_test() {
+        let sample = String::from(":dan!d@localhost PRIVMSG #chan Hey!\r\n");
 
-        let msg = RawMsg::decode(sample);
+        let msg = RawMsg::from_string(sample);
 
         assert_eq!(None, msg.tags);
         assert_eq!("dan!d@localhost", msg.source.unwrap());
@@ -143,4 +183,55 @@ mod tests {
         assert_eq!("Hey!", msg.params[1]);
         assert_eq!(2, msg.params.len());
     }
+
+    #[test]
+    fn to_string_simple_test() {
+        let sample = RawMsg{
+            tags: None, 
+            source: None, 
+            command: "PRIVMSG".to_string(), 
+            params: vec![
+                "#chan".to_string(), 
+                "Hello world!".to_string()
+            ]
+        };
+
+        assert_eq!("PRIVMSG #chan :Hello world!\r\n", sample.to_string());
+    }
+
+    #[test]
+    fn to_string_source_test() {
+        let sample = RawMsg{
+            tags: None, 
+            source: Some("dan!d@localhost".to_string()), 
+            command: "PRIVMSG".to_string(), 
+            params: vec![
+                "#chan".to_string(), 
+                "Hello world!".to_string()
+            ]
+        };
+
+        assert_eq!(":dan!d@localhost PRIVMSG #chan :Hello world!\r\n", sample.to_string());
+    }
+
+    #[test]
+    fn to_string_complete_test() {
+        let tags: BTreeMap<String, Option<String>> = vec![
+            ("id".to_string(), Some("234AB".to_string())),
+            ("rose".to_string(), None),
+        ].into_iter().collect();
+
+        let sample = RawMsg{
+            tags: Some(tags), 
+            source: Some("dan!d@localhost".to_string()), 
+            command: "PRIVMSG".to_string(), 
+            params: vec![
+                "#chan".to_string(), 
+                "Hello world!".to_string()
+            ]
+        };
+
+        assert_eq!("@id=234AB;rose :dan!d@localhost PRIVMSG #chan :Hello world!\r\n", sample.to_string());
+    }
+
 }
